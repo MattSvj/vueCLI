8529be271a162a93cce13d876d5780ea4fbae01a
Matt <matthew-03@mail.ru>
2024-07-18 17:46:01 +0300

    sem4

---
+++ vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <VueBlog/>
+  </div>
+</template>
+
+<script>
+import VueBlog from './components/Blog.vue';
+
+export default {
+  name: 'App',
+  components: {
+    VueBlog
+  }
+}
+</script>
+
+<style>
+
+</style>
